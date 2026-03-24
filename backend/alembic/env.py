019e0de2cacd2b509df135b477f074397af9531a
Siddharta Govindaraj <siddharta@silverstripesoftware.in>
2026-03-24 13:04:47 +0530

    Updated alembic configuration

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -16,8 +16,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from backend.app.database import Base
-from backend.app.models import Customer, Vendor
+from app.database import Base
+from app.models import Customer, Vendor
 
 target_metadata = Base.metadata
 
